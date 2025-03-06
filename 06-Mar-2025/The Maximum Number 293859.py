c48f1b23303f07147552ba7439fe9fff7f39e53b
eyu birhanu <eyubirhanu01@gmail.com>
2025-03-06 07:33:29 +0300

    Created 'The Maximum Number' by A2SV Hub

---
+++ 06-Mar-2025/The Maximum Number 293859.py
@@ -0,0 +1,11 @@
+# Problem: The Maximum Number - https://leetcode.com/problems/third-maximum-number/description/
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        numbers = list(set(nums))
+        numbers.sort()
+        
+        if len(numbers) < 3:
+            return numbers[-1]
+        else:
+            return numbers[-3]
