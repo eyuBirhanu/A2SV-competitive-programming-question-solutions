c66d198677f7a0f9219abb1ebb7bc51080ec5797
eyu birhanu <eyubirhanu01@gmail.com>
2025-03-10 04:00:49 +0300

    Created 'Valid Anagram' by A2SV Hub

---
+++ 10-Mar-2025/Valid Anagram 299185.py
@@ -0,0 +1,5 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return True if sorted(s) == sorted(t) else False 
