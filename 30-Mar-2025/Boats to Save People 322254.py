0f18cc68141501251830e8fd0c43a63acb25fa23
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-03-30 13:37:16 -0700

    Created 'Boats to Save People' by A2SV Hub

---
+++ 30-Mar-2025/Boats to Save People 322254.py
@@ -0,0 +1,33 @@
+# Problem: Boats to Save People - https://leetcode.com/problems/boats-to-save-people/
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort(reverse=True)
+        mass = sum(people)
+        sum_ = 0
+        boat = 0
+        i = 0
+        j = len(people)-1
+        while i < j and i < len(people):
+            if people[i] == limit:
+                boat += 1
+                sum_ += people[i]
+                i += 1
+            elif people[i] < limit:
+                limit_ = people[i]
+                if limit_ + people[j] > limit:
+                    boat += 1
+                    sum_ += people[i]
+                    i += 1
+                elif limit_ + people[j] <= limit:
+                    boat += 1
+                    sum_ += (people[i] + people[j])
+                    i += 1
+                    j -= 1
+            elif people[i] > limit:
+                i += 1
+        if sum_ < mass:
+            boat += 1
+        return boat
+                
+                    
