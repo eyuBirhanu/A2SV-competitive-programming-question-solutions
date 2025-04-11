bc5a5272da0c4dc2070be875aec09998f65c5b1b
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-04-11 00:22:46 -0700

    Created 'Longest Palindromic Substring' by A2SV Hub

---
+++ 11-Apr-2025/Longest Palindromic Substring 331493.py
@@ -0,0 +1,34 @@
+# Problem: Longest Palindromic Substring - https://leetcode.com/problems/longest-palindromic-substring/description/
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ''
+        if len(s) == 1:
+            return s
+        for i in range(len(s)):
+            if i < 1:
+                res = s[:i+1]
+            elif i > 0:
+                l, r = i-1, i+1
+                while l >= 0 and r < len(s):
+                    if s[l] == s[r]:
+                        sub_res = s[l:r+1]
+                        if len(sub_res) > len(res):
+                            res = sub_res
+                        l -= 1
+                        r += 1
+                    else:   
+                        break
+                l, r = i-1, i
+                while l >= 0 and r < len(s):
+                    if s[l] == s[r]:
+                        sub_res = s[l:r+1]
+                        if len(sub_res) > len(res):
+                            res = sub_res
+                        l -= 1
+                        r += 1
+                    else:   
+                        break
+                
+        return res
+                
