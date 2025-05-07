88c3157208bacc3a954a3b35d1eed9bf938253f3
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-05-07 21:10:58 +0300

    Created 'Find Peak Element' by A2SV Hub

---
+++ 07-May-2025/Find Peak Element 350442.py
@@ -0,0 +1,5 @@
+# Problem: Find Peak Element - https://leetcode.com/problems/find-peak-element
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+            return nums.index(max(nums))
