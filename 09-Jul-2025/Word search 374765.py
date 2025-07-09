d8bf537c093b755595e23f9d5bb4d0fb0a7abb8b
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-07-09 02:31:04 -0700

    Created 'Word search' by A2SV Hub

---
+++ 09-Jul-2025/Word search 374765.py
@@ -0,0 +1,32 @@
+# Problem: Word search - https://leetcode.com/problems/word-search/
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        row = len(board)
+        col = len(board[0]) 
+        visit = set()
+
+        def check(r, c, curr):
+            if curr == len(word):
+                return True
+
+            if (r < 0 or c < 0 or
+                r >= row or c >= col or
+                (r, c) in visit or
+                board[r][c] != word[curr]):
+                return False
+                
+            visit.add((r, c))
+            res = (check(r+1, c, curr+1) or
+                    check(r-1, c, curr+1) or
+                    check(r, c+1, curr+1) or
+                    check(r, c-1, curr+1))
+            visit.remove((r,c))
+            return res
+
+
+        
+        for r in range(row):
+            for c in range(col):
+                if check(r, c, 0): return True
+        return False
