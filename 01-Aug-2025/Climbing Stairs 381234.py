fddbf350eb333eeec729af3e922412fc3c9a014a
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-08-01 12:29:06 -0700

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 01-Aug-2025/Climbing Stairs 381234.py
@@ -0,0 +1,14 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        cnt = 0
+        def dp(n):
+            if n in memo:
+                return memo[n]
+            if n == 0 or n == 1:
+                return 1
+            memo[n] = dp(n-1) + dp(n-2) 
+            return memo[n]
+        return dp(n)
