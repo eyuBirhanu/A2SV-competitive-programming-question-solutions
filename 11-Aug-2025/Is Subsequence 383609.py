aba578bdf19e749f7973bea161f2458ff2733717
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-08-11 11:24:42 -0700

    Created 'Is Subsequence' by A2SV Hub

---
+++ 11-Aug-2025/Is Subsequence 383609.py
@@ -0,0 +1,16 @@
+# Problem: Is Subsequence - https://leetcode.com/problems/is-subsequence/
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True 
+        idx = 0
+        cnt = 0
+        for i in range(len(t)):
+            if idx >= len(s):
+                return True
+            if t[i] == s[idx]:
+                cnt += 1
+                idx += 1
+        return cnt == len(s)
+            
