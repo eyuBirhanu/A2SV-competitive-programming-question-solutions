51f7b409ce06aa256379f6b00722b6c3aad76e3e
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-08-14 11:14:06 -0700

    Created 'Target Sum' by A2SV Hub

---
+++ 14-Aug-2025/Target Sum 385103.py
@@ -0,0 +1,19 @@
+# Problem: Target Sum - https://leetcode.com/problems/target-sum/
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = {}
+        def dfs(idx, total):
+            if idx == len(nums):
+                if total  == target:
+                    return 1
+                return 0
+
+            if (idx, total) in dp:
+                return dp[(idx, total)]
+
+            dp[(idx, total)] = (dfs(idx+1, total+nums[idx]) + 
+                                dfs(idx+1, total-nums[idx]))
+
+            return dp[(idx, total)]
+        return dfs(0, 0)
