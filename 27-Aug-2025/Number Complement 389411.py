288488f2766b6023f77858ecc19b197ab3c13ed3
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-08-27 23:10:46 +0300

    Created 'Number Complement' by A2SV Hub

---
+++ 27-Aug-2025/Number Complement 389411.py
@@ -0,0 +1,7 @@
+# Problem: Number Complement - https://leetcode.com/problems/number-complement/
+
+class Solution:
+    def findComplement(self, num: int) -> int:
+        n = ((num).bit_length())
+        mask = (1 << n) - 1
+        return num ^  mask
