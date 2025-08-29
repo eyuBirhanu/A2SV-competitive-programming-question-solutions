e644ffb5c4aa0d9b163d2a1f7c657d4ca14d3950
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-08-30 01:59:29 +0300

    Created ' Add Binary' by A2SV Hub

---
+++ 29-Aug-2025/ Add Binary 390074.py
@@ -0,0 +1,23 @@
+# Problem:  Add Binary - https://leetcode.com/problems/add-binary/
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        if a == '0' and b == '0' :
+            return '0'
+        def toBi(str_):
+            x = 0
+            cnt = len(str_)-1
+            for i in str_:
+                x += (2**cnt) * int(i)
+                cnt -= 1
+            return x
+        
+        a = toBi(a)
+        b = toBi(b)
+        c = a + b
+        res = ''
+
+        while c > 0 :
+            res += str(c%2)
+            c //= 2
+        return res[::-1]
