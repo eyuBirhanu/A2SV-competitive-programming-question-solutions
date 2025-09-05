d8fe825551edfc1490414295ef3b695797ee70aa
Eyu Birhanu <eyubirhanu01@gmail.com>
2025-09-05 12:51:52 -0700

    Created 'Missing Number' by A2SV Hub

---
+++ 05-Sep-2025/Missing Number 391500.py
@@ -0,0 +1,12 @@
+# Problem: Missing Number - https://leetcode.com/problems/missing-number/description/
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        sorted_nums = sorted(nums)
+        range_ = []
+        for i in range(0, len(sorted_nums) + 1):
+           range_.append(i)
+        #    1qwqw
+        for i in range_:
+            if i not in sorted_nums:
+                return i
